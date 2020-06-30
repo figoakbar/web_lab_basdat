88e887edb3d5fa8c374d1f0c907313eab49e8bcd
Figo Akbar <figoakbar15@gmail.com>
2020-06-30 00:56:45 -0700

    Update Location.php

--- Location.php
+++ Location.php
@@ -91,26 +91,26 @@
 
 </style>
 <body>
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">My Booking</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
-                            role="button"><img src="./account.svg" alt="" height="40px" width="40px"></a>
-                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left">
-                                    <a class="dropdown-item" href="#">Profile</a>
-                                    <a class="dropdown-item" href="Logout.php">logout</a>
-                                </div>
-                        </li>
-                        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Booking</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"role="button">
+                    <img src="./account.svg" alt="" height="40px" width="40px"></a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="Logout.php">logout</a>
+                        </div>
+                </li>
+            </ul>
                     </div>
                 </nav>
     <h2 class= "title">TRAVELY<h2>
