16442e08ddc44b5d2afe62700867fc5a8dcc7642
Figo Akbar <figoakbar15@gmail.com>
2020-06-30 08:21:14 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,14 @@
 <?php
-    $mysqli = new mysqli("localhost","root","","travely");
-    if(mysqli_connect_errno()){
-        printf("Connect failed: %s\n",mysqli_connect_error());
-        exit();
-    }
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "travely";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
 ?>
