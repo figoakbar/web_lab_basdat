96b1ec71b6a4d20e7de9b1e55ada3f32f97e4512
Figo Akbar <figoakbar15@gmail.com>
2020-07-10 06:39:49 -0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -139,9 +139,6 @@ if(isset($_POST['login'])){
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">About Us</a>
                         </li>
