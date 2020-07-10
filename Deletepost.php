3c20fa52897244cbf965b23408edcdca0f277eb8
Figo Akbar <figoakbar15@gmail.com>
2020-07-10 10:16:36 -0700

    Update Deletepost.php

--- Deletepost.php
+++ Deletepost.php
@@ -4,7 +4,7 @@
     $result = mysqli_query($db,"DELETE FROM post WHERE id_post = $_GET[post]");
     if($result){
         echo "<script>alert('Your post was successfully Deleted'); 
-        window.location = 'Home.php';</script>";
+        window.location = 'Myposts.php';</script>";
     } else {
         echo "<script>alert('Try Again!'); 
         window.location = 'Myposts.php';</script>";
