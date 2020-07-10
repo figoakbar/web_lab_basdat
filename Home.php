2cab171d87cd1d74bb320e51e48685fe61594be2
Figo Akbar <figoakbar15@gmail.com>
2020-07-10 06:39:43 -0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -168,7 +168,7 @@
                             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">My Booking</a>
+                            <a class="nav-link" href=" Myposts.php?id_user=<?php echo  $_SESSION['user']['id_user'] ?>">My Posts</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">About Us</a>
